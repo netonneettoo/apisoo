b0c0c16778dee24dbefacbc3231d0c8e5e5b845f
Walter Neto <walter@csktech.com.br>
2016-06-01 01:01:31 -0300

    add model and migration de teachers

---
+++ database/migrations/2016_06_01_035746_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->unique();
+            $table->string('cpf', 14)->unique();
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teachers');
+    }
+}
