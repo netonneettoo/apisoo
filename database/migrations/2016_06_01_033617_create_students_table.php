29b61f2a5ad4df38fb6e618ba5390063af00cb1b
Walter Neto <walter@csktech.com.br>
2016-06-01 00:57:22 -0300

    add student, model and migration

---
+++ database/migrations/2016_06_01_033617_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->unique();
+            $table->unsignedBigInteger('registration')->unique();
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
