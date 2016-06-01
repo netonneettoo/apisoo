694a320857a31b896e3e70f773ad5df340dc2cdb
Walter Neto <walter@csktech.com.br>
2016-06-01 01:15:04 -0300

    ad model e migration de turmas

---
+++ database/migrations/2016_06_01_040537_create_discipline_classes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDisciplineClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discipline_classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('discipline_id');
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedSmallInteger('year');
+            $table->enum('half', [1, 2]);
+            $table->timestamps();
+
+            $table->foreign('discipline_id')->references('id')->on('disciplines');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+
+            $table->unique(['discipline_id', 'teacher_id', 'year', 'half']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('discipline_classes');
+    }
+}
