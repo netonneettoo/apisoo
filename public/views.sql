bdf2194cd9281c53965681d28ddadaba362ebb35
Walter Neto <walter@csktech.com.br>
2016-06-02 09:39:13 -0300

    fazendo querys complexas para mostrar melhor os resultados

--- public/views.sql
+++ public/views.sql
@@ -1,4 +1,4 @@
--- turmas
+-- discipline_classes (turmas)
 select
   dc.id,
   d.description as discipline,
@@ -9,3 +9,14 @@ from discipline_classes dc
 inner join disciplines d on dc.discipline_id = d.id
 inner join teachers t on dc.teacher_id = t.id
 inner join users u on t.user_id = u.id
+
+-- course_disciplines
+select
+  cd.id,
+  c.id as course_id,
+  c.description as course_description,
+  d.id as discipline_id,
+  d.description as discipline_description
+from course_disciplines cd
+inner join courses c on cd.course_id = c.id
+inner join disciplines d on cd.discipline_id = d.id
